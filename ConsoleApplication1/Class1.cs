33fb82526fb6caa902a31337b5260f62b411353f
Igor Trafalski <igorek2691@gmail.com>
2017-11-27 19:54:31 +0100

    Add project files.

---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Entity
+    {
+        public string Name { get; set; }
+        public bool HasPriority { get; set; }
+        public bool HasIssue { get; set; }
+
+        string[] allEntities = new[] { "a", "b", "c", "d", "e", "f", "g", "h", "i" };
+        string[] entitiesWithPriority = new[] { "c", "d", "g" };
+        string[] entitiesWithIssue = new[] { "d", "h", "i" };
+
+        public List<List<Dictionary<string, dynamic>>> MethodName()
+        {
+            var list = new List<List<Dictionary<string, dynamic>>>();
+
+            HasPriority = false;
+            HasIssue = false;
+
+            foreach (var item in allEntities)
+            {
+                if (entitiesWithPriority.Contains(item.ToString()))
+                    HasPriority = true;
+                if (entitiesWithIssue.Contains(item.ToString()))
+                    HasIssue = true;
+                list.Add(new List<Dictionary<string, dynamic>>()
+                {
+                    (new Dictionary<string, dynamic>() {
+                        { "name", item },
+                        { "hasPriority", HasPriority },
+                        { "hasIssue", HasIssue }
+                    })
+                });
+            }
+            return list;
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new Entity();
+            var list = instance.MethodName();
+
+            foreach (var item in list)
+            {
+                foreach (var item1 in item)
+                    foreach (var item2 in item1)
+                        Console.WriteLine($"{item2.Key}, {item2.Value}");
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
